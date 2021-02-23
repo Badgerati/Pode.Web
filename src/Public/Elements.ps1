e8d5fcbe36e874b0f4d463a0f191ff163b75d671
Matthew Kelly <badgerati42@gmail.com>
2021-02-23 21:31:05 +0000

    #27: fix for auto-wrapping table/chart outputs

--- src/Public/Elements.ps1
+++ src/Public/Elements.ps1
@@ -1352,7 +1352,7 @@ function New-PodeWebChart
                 $result = @()
             }
 
-            if (($result.Length -gt 0) -and [string]::IsNullOrWhiteSpace($result[0].OutputType)) {
+            if (!(($result -is [hashtable]) -and ($result.Operation -ieq 'Output') -and ($result.ElementType -ieq 'Chart'))) {
                 $result = ($result | Out-PodeWebChart -Id $using:Id)
             }
 
@@ -1577,7 +1577,7 @@ function New-PodeWebTable
                 $result = @()
             }
 
-            if (($result.Length -gt 0) -and [string]::IsNullOrWhiteSpace($result[0].OutputType)) {
+            if (!(($result -is [hashtable]) -and ($result.Operation -ieq 'Output') -and ($result.ElementType -ieq 'Table'))) {
                 $paginate = $ElementData.Paging.Enabled
                 $result = ($result | Out-PodeWebTable -Id $using:Id -Columns $ElementData.Columns -Paginate:$paginate)
             }
