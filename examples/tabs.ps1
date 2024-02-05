d26602120a21fd9d7a1ca3d5b091ac86402b337c
Staples1887 <59692325+Staples1887@users.noreply.github.com>
2024-02-05 09:37:22 +0000

    Added Tabs Example

---
+++ examples/tabs.ps1
@@ -0,0 +1,31 @@
+Import-Module Pode -MaximumVersion 2.99.99 -Force
+Import-Module ..\src\Pode.Web.psm1 -Force
+
+Start-PodeServer {
+    # add a simple endpoint
+    Add-PodeEndpoint -Address localhost -Port 8090 -Protocol Http
+    New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
+
+    # set the use of templates, and set a login page
+    Use-PodeWebTemplates -Title 'Basic tabs' -Theme Dark
+    
+    $tabs = New-PodeWebTabs -Tabs @(
+        New-PodeWebTab -Name Tab1 -Content @(
+            New-PodeWebContainer -Content @(
+                New-PodeWebText -Value 'Hello World'
+            )
+        )
+        New-PodeWebTab -Name Tab2 -Content @(
+            New-PodeWebContainer -Content @(
+                New-PodeWebText -Value 'Hello There'
+            )
+        )
+        New-PodeWebTab -Name Tab3 -Content @(
+            New-PodeWebContainer -Content @(
+                New-PodeWebText -Value 'HI!'
+            )
+        )
+    )
+    
+    Set-PodeWebHomePage -Content $tabs -Title 'Tabs'
+}
