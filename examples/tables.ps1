b7b58a3718f3a4ef301d7c055cabccb7b939b562
Birk Føste <Birk@Foste.IT>
2026-03-16 00:56:23 +0100

    Add MultiSelect option to New-PodeWebTable function

--- examples/tables.ps1
+++ examples/tables.ps1
@@ -57,5 +57,63 @@ Start-PodeServer -Threads 2 {
         )
     )
 
-    Add-PodeWebPage -Name 'Home' -Path '/' -HomePage -Content $card1, $card2 -Title 'Tables'
+    $multiTable = New-PodeWebTable `
+        -Name 'MultiSelect' `
+        -PageSize 4 `
+        -Paginate `
+        -Filter `
+        -SimpleFilter `
+        -Compact `
+        -DataColumn 'ID' `
+        -MultiSelect `
+        -ScriptBlock {
+        $allProcesses = @(Get-Process | ForEach-Object {
+                [ordered]@{
+                    Name       = $_.Name
+                    ID         = $_.Id
+                    WorkingSet = $_.WorkingSet
+                    CPU        = $_.CPU
+                }
+            })
+
+        $totalCount = $allProcesses.Count
+        $pageIndex = [int]$WebEvent.Data.PageIndex
+        $pageSize = [int]$WebEvent.Data.PageSize
+        $processes = $allProcesses[(($pageIndex - 1) * $pageSize) .. (($pageIndex * $pageSize) - 1)]
+
+        $processes | Update-PodeWebTable -Name $ElementData.Name -PageIndex $pageIndex -TotalItemCount $totalCount
+    } `
+        -Columns @(
+        Initialize-PodeWebTableColumn -Key 'Name'
+        Initialize-PodeWebTableColumn -Key 'ID'
+        Initialize-PodeWebTableColumn -Key 'WorkingSet' -Name 'Memory'
+        Initialize-PodeWebTableColumn -Key 'CPU' -Hide
+    )
+
+    $multiTable | Add-PodeWebTableButton -Name 'StopSelected' -DisplayName 'Stop Selected' -Icon 'delete' -WithText -ScriptBlock {
+        $selected = $WebEvent.Data['Selection'] -split ','
+        if ($selected.Length -eq 0) {
+            Show-PodeWebToast -Message 'No processes selected' -Title 'MultiSelect' -Duration 3000
+        }
+        else {
+            Show-PodeWebModal -Name 'StopSelected' -Actions @(
+                Update-PodeWebTextbox -Name 'SelectedProcesses' -Value ($selected -join "`n")
+            )
+        }
+    }
+
+    $stopModal = New-PodeWebModal -Name 'StopSelected' -DisplayName 'Stop Selected Processes' -Size 'Medium' -AsForm -Content @(
+        New-PodeWebTextbox -Name 'SelectedProcesses' -DisplayName 'Selected Process IDs' -Multiline -ReadOnly
+    ) -ScriptBlock {
+        $ids = ($WebEvent.Data['SelectedProcesses'] -split "`n") | Where-Object { ![string]::IsNullOrWhiteSpace($_) }
+        foreach ($id in $ids) {
+            Stop-Process -Id ([int]$id) -Force -ErrorAction SilentlyContinue
+        }
+        Show-PodeWebToast -Message "Stopped $($ids.Count) process(es)" -Title 'Done' -Duration 3000
+        Hide-PodeWebModal
+    }
+
+    $card3 = New-PodeWebCard -Name 'MultiSelect Processes' -Content $multiTable
+
+    Add-PodeWebPage -Name 'Home' -Path '/' -HomePage -Content $card1, $card2, $card3, $stopModal -Title 'Tables'
 }
